2c0b870898a51ca492aede85d43c38963106c555
Viet Nguyen Trong Hoang <vieritolove@gmail.com>
2018-01-02 11:44:38 +0700

    update IMP interpreter

---
+++ imp-interpreter/imp.py
@@ -0,0 +1,32 @@
+import sys
+from imp_parser import *
+from imp_lexer import *
+
+def usage():
+    sys.stderr.write('Usage: imp filename\n')
+    sys.exit(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+    filename = sys.argv[1]
+    text = open(filename).read()
+    print("##################### CODE ###########################")
+    print(text)
+    print("##################### TOKENS ###########################")
+    tokens = imp_lex(text)
+    print(tokens)
+    print("##################### PARSER ###########################")
+    parse_result = imp_parse(tokens)
+    print(parse_result)
+    print("##################### RESULT ###########################")
+    if not parse_result:
+        sys.stderr.write('Parse error!\n')
+        sys.exit(1)
+    ast = parse_result.value
+    env = {}
+    ast.eval(env)
+
+    sys.stdout.write('Final variable values:\n')
+    for name in env:
+        sys.stdout.write('%s: %s\n' % (name, env[name]))
